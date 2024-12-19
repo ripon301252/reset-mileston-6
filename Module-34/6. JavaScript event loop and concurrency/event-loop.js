9deef0bebf7a43483a426290658a05d47622eff1
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2024-12-20 03:16:22 +0600

    Update event-loop.js

--- Module-34/6. JavaScript event loop and concurrency/event-loop.js
+++ Module-34/6. JavaScript event loop and concurrency/event-loop.js
@@ -12,7 +12,34 @@ function b(){
 
 function d(){
     console.log('d')
+    x()
     console.log('Dd')
 }
 
-a();
+function x(){
+    console.log('x')
+    y()
+    console.log('xx')
+}
+
+function y(){
+    console.log('y')
+    z()
+    console.log('yy')
+}
+
+function z(){
+    console.log('z')
+    console.log('zz')
+}
+
+setTimeout(() => {
+    console.log('inside time')
+}, 2000)
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+a(); // call stack
+
